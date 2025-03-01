97874f8431bd83d16dce5cae9daa6769b0754d86
Mironlay <mirocom3443@gmail.com>
2025-03-01 22:39:05 +0200

    js completed

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const age = 17;
+console.log(age);
+const name = "Miroslav";
+console.log(name);
+let isStudent = true
+console.log(isStudent);
+const myString = "I tried to scream, but my head was underwater";
+console.log(myString);
+const myNumber = 7;
+console.log(myNumber + 10);
+const myNull = null
+console.log(myNull);
+const userName = prompt("Введіть своє ім’я");
+console.log(typeof userName);
+alert(userName);
+const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log(userAnswer);
+const warn = alert("Увага! Дія може бути небезпечною.");
+console.log(warn);
+const agree = confirm("Ви впевнені, що хочете продовжити?")
+console.log(agree);
